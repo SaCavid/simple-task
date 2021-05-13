8811d2e15e723210f0cf3e1ca1f59a28bf0e030f
SaCavid <sadiqov.cavid@gmail.com>
2021-05-13 23:43:24 +0400

    Finishing

--- pro.bat
+++ pro.bat
@@ -4,8 +4,6 @@ git commit -m "Finishing"
 PAUSE
 git push origin master
 PAUSE
-ECHO Congratulations! Your first batch file executed successfully.
-PAUSE
 docker image build -t sacavid/simple-task .
 PAUSE
 docker push sacavid/simple-task:latest
